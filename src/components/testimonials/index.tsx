da37938b442f81b503bb4ef42cc2f09d63cad689
kjsingh03 <k.jsingh.12003@gmail.com>
2025-08-08 20:09:57 +0530

    first commit

--- src/components/testimonials/index.tsx
+++ src/components/testimonials/index.tsx
@@ -1,6 +1,6 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
-import 'swiper/css';
+// import 'swiper/css';
 import { useState } from 'react';
 import SwiperCore from 'swiper';
 import { testimonial } from '../../assets';
